b7ffcf7fa523cb58266178610d479b66db862924
atlas21_se04 <watarusatoh60@gmail.com>
2019-01-21 14:33:06 +0900

    GetAPIへWHERE.

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -34,7 +34,7 @@ class Api::V1::CharactersController < ApplicationController
   end
 
   def getWhereUpdate
-    @characters = ActiveRecord::Base.connection.execute("SELECT * FROM characters WHERE to_date(up_date, 'YYYYMMDD') >= to_date('#{param[:fromUpdate]}', 'YYYYMMDD')")
+    @characters = ActiveRecord::Base.connection.execute("SELECT * FROM characters WHERE to_date(up_date, 'YYYYMMDD') >= to_date('#{params[:fromUpdate]}', 'YYYYMMDD')")
     @characters = @characters.map do |character|
       {
         No: character.id,
